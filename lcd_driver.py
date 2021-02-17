178151cb3197dc3f201a390f65dce2fdf80e2ccc
Rik Beernink <rik@rikbeernink.nl>
2021-02-17 19:02:40 +0100

    Rewrote main.py to write text to LCD in all cases.
    Removed blue LED and associated code.
    
    renamed vibration_sensor to magnetic_switch to be used with the new magnetic switch
    
    in lcd_driver.py removed word-wrap function. Now manually correct text.

--- lcd_driver.py
+++ lcd_driver.py
@@ -3,7 +3,6 @@ import board
 import digitalio
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
-from time import sleep
 
 # Setting some variables for our reset pin etc.
 RESET_PIN = digitalio.DigitalInOut(board.D4)
@@ -12,25 +11,28 @@ RESET_PIN = digitalio.DigitalInOut(board.D4)
 i2c = board.I2C()
 display = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3C, reset=RESET_PIN)
 
+display_status = 0
+
+
 def clear_display():
     display.fill(0)
     display.show()
 
 
-def draw_text(text, size, screentime):
+def draw_text(text, size="small"):
     display.fill(0)
     display.show()
     if size is "small":
         font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 10)
+
     elif size is "big":
-        font2 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 28)
+        font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 28)
 
     image = Image.new("1", (display.width, display.height))
     draw = ImageDraw.Draw(image)
-    draw.text((0, 32), text, font=font, fill=255)
+    draw.text((0, 0), text, font=font, fill=255)
 
     display.image(image)
     display.show()
-
-    sleep(screentime)
-    clear_display()
+    global display_status
+    display_status = 1
--- main.py
+++ main.py
@@ -5,16 +5,15 @@ import pathlib
 import datetime
 import shutil
 import signal
-import lcd_driver
+import lcd_driver as display
 
 from gpiozero import Button, LED, MotionSensor
 
 scan_button = Button(17)  # button used to scan the IR signal to be sent to the AC unit
 test_button = Button(27)  # button used to send the IR signal (for testing only)
 red_led = LED(16)
-blue_led = LED(12)
-vibration_sensor = MotionSensor(25)  # Using MotionSensor gpiozero interface for vibration_sensor due to lack of vibration sensor interface in this package.
-motion_sensor = MotionSensor(26)
+magnetic_switch = MotionSensor(25)  # Using MotionSensor gpiozero interface for magnetic_switch due to lack of vibration sensor interface in this package.
+motion_sensor = MotionSensor(8)
 
 pwd = str(pathlib.Path(__file__).parent.absolute())  # determines the present working directory (PATH) to determine where to load and store the captured key files
 captured_key_file_location = pwd + '/captured_key.txt'  # name given to the captured key file
@@ -37,16 +36,6 @@ def blink_2_slow(led_light):
         sleep(0.4)
 
 
-def dual_blink_2_slow(led_light1, led_light2):
-    for x in range(0, 2):
-        led_light1.on()
-        led_light2.on()
-        sleep(0.4)
-        led_light1.off()
-        led_light2.off()
-        sleep(0.4)
-
-
 def shutdown_ac():  # function that sends the IR code (testing only?)
     try:
         if os.stat(captured_key_file_location).st_size < 10:  # checks if code has been scanned. if not raises an error.
@@ -57,10 +46,8 @@ def shutdown_ac():  # function that sends the IR code (testing only?)
         if 'failed' in result:
             print('IR SEND FAILED')
             blink_2_slow(red_led)
-            blink_2_slow(blue_led)
     except OSError:
         print('No code found. Please scan first')
-        dual_blink_2_slow(red_led, blue_led)
 
 
 def check_airco_off():  # function that checks if the airco is shutdown correctly. If not, it will attempt to shutdown the airco again.
@@ -70,7 +57,7 @@ def check_airco_off():  # function that checks if the airco is shutdown correctl
     while i < 20:
         sleep(1)
         now = datetime.datetime.now()
-        if vibration_sensor.is_active:
+        if not magnetic_switch.is_active:
             vibration_time_limit = set_time_limit(now, 'seconds', 10)
         if i == 19:
             if now.time() < vibration_time_limit:
@@ -89,10 +76,15 @@ def check_airco_off():  # function that checks if the airco is shutdown correctl
 def scan_code():  # activates the scanner for 5 seconds. Press remote button once to scan and save it.
     if os.path.exists(captured_key_file_location):  # checks if a code already exists, if so backs it up and removes the present one
         print('Backing up old remote configuration..')
+        display.draw_text("Old IR code found...\nBacking up old key.", "small")
         shutil.copyfile(captured_key_file_location, backup_file)  # backup old key
+        sleep(2)
         os.remove(captured_key_file_location)  # remove old key (the NON backup)
+        display.draw_text("Backup completed.", "small")
+        sleep(2)
 
     print('Scanner activated.\n')
+    display.draw_text("Scanner activated.\n\nPress off switch on \nremote controller now.")
     red_led.on()  # red LED on to indicate you should scan the code now
     command_string = "sudo ir-ctl --mode2 -d /dev/lirc1 -r > " + captured_key_file_location  # string to be used to activate the IR receiver and the associate capture  file name
     cmd = subprocess.Popen(command_string, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)  # executes command to activate the IR receiver to scan and save IR code.
@@ -101,17 +93,21 @@ def scan_code():  # activates the scanner for 5 seconds. Press remote button onc
     red_led.off()
     if os.stat(captured_key_file_location).st_size > 200:
         print('Scan successful! Remote captured')
+        display.draw_text("Scan successful!\nIR key saved to file.")
+        sleep(2)
         try:
             os.remove(backup_file)
             print('Cleaning up backup file.')
         except OSError:
             pass
-        blink_10_fast(blue_led)
         return True
     else:
         print('Scan failed')  # following logic restores backup key if no key has been scanned
+        display.draw_text("Scan failed!")
         os.remove(captured_key_file_location)
+        sleep(2)
         if os.path.exists(backup_file):
+            display.draw_text("Restoring backup-key.")
             shutil.copyfile(backup_file, captured_key_file_location)
             os.remove(backup_file)
             print('Restoring backup.')
@@ -127,6 +123,7 @@ def set_time_limit(time_object, time_type, time_to_add):  # ads a certain time t
 
 
 def airco_running():  # Vibration has been detected. It has been determined the airco is running. This function checks if the airco can be on with or without limitations. If so it will enforce said limitations
+    display.display_status = 1  # arms the display to be reset later on.
     now = datetime.datetime.now().time()
     start = datetime.time(
         8)  # beginning of the time period where the airco can NOT be on unrestricted. (Format (hours, minutes) i.e.: datetime.time(8, 30) is 08:30 AM.)
@@ -134,7 +131,7 @@ def airco_running():  # Vibration has been detected. It has been determined the 
         22)  # ending of the time period where the airco can NOT be on unrestricted.     (Format (hours, minutes) i.e.: datetime.time(22, 15) is 10:15 PM.)
 
     if now < start or now > end:  # Checks if the airco is in the "unrestricted time period". If so, we will not continue further.
-        blink_10_fast(blue_led)
+        display.draw_text("Airco is allowed to be on.\nExiting restrictive schedule.")
         return None  # skip function
 
     last_motion = datetime.datetime.now()  # The last motion variable is a date-time (time) variable which contains the last point in time MOTION has been detected. It is set to the current time now for initialisation.
@@ -150,13 +147,15 @@ def airco_running():  # Vibration has been detected. It has been determined the 
 
     print('In restricted time. Starting airco control script: ')  # for debugging only
     while True:
-        blue_led.on()
+        if display.display_status == 1:
+            display.draw_text("Monitoring for movement\n\nA/C will shutdown at:\n" + str(time_limit))
+            display.display_status = 0
         if now < start or now > end:  # This once again checks if since the time the airco started, we have now entered the "unrestricted" time span. In this case the script is no longer needed.
-            blink_10_fast(blue_led)
             break
 
         if motion_sensor.is_active:  # Motion is detected. time_limit variable will be set to current time + 'airco_run_limit' variable. This will be the new time where the AC will be shutoff if no motion detected.
             print('Motion detected')  # for debugging only
+            display.draw_text("Motion detected!\n\n Resetting timer.")
             red_led.on()  # for debugging only
             sleep(1)  # for debugging only
             red_led.off()  # for debugging only
@@ -165,51 +164,57 @@ def airco_running():  # Vibration has been detected. It has been determined the 
             print(f'Time limit reset to {time_limit}')  # debugging only
             continue  # skip rest of the loop because unnecessary because movement has been detected.
 
-        if vibration_sensor.is_active:
+        if not magnetic_switch.is_active:
             vibration_time_limit = set_time_limit(datetime.datetime.now(), 'seconds', vibration_limit)
             #  print('Vibration detected')  # debugging only
 
         now = datetime.datetime.now().time()
         if now > time_limit:  # Airco has been on without detected movement for longer than the allowed limit.
             print(f'No motion for the last {airco_run_limit} minutes.')
+            display.draw_text(f'No motion for the last\n{airco_run_limit} minutes.\n\nAttempting shutdown.')
             if now < vibration_time_limit:  # If the airco has been registered on the last 10 seconds. We will not shutdown the AC (to prevent accidentally starting the AC instead of stopping it)
                 print('Attempting to shut down the AC')
                 shutdown_ac()  # send A/C off signal.
                 sleep(10)
                 check_airco_off()  # check if airco shut down. If not, this function will restart.
                 print('Airco is off')  # for debugging only
+                display.draw_text('A/C shut down.')
             else:
                 print('Airco is already off.')
-                blink_2_slow(blue_led)
-            blue_led.off()
+                display.draw_text("Airco already off.")
             break
 
         sleep(0.5)
 
 
 def main():
-    lcd_driver.draw_text("Welcome to AircoPi", "small", 10)
+    display.draw_text("Welcome to AircoPi", "small")
     if not os.path.exists(captured_key_file_location):  # checks if an IR key to shut down the AC has already been captured.
-        dual_blink_2_slow(blue_led, red_led)  # blinks both LED's twice to notify the user there is no IR key captured.
+        display.draw_text("No key found.\nPress button when ready\nto scan the IR signal.", "small")
         now = datetime.datetime.now()  # saves current time
         flash_limit = 10  # interval between led flashes in seconds (to remind user he needs to capture key)
         blink_limit = set_time_limit(now, 'seconds', flash_limit)  # sets a date-time object after which the LED's will flash again.
 
         while True:  # enters a loop to ensure an IR code is captured and saved before the rest of the script can be run.
+
             now = datetime.datetime.now()
             if scan_button.is_pressed:  # allows for scanning an IR code.
                 scan_result = scan_code()
                 if scan_result:  # if the scan function returns True, a successful key capture. The loop may be exited.
                     break
             if now.time() > blink_limit:  # checks if the LED's need to be flashed again
-                dual_blink_2_slow(blue_led, red_led)
+                display.draw_text("No key found.\nPress button when ready\nto scan the IR signal.", "small")
                 blink_limit = set_time_limit(now, 'seconds', flash_limit)  # sets new time point to flash LED's
 
-    blink_2_slow(blue_led)
+
     while True:
+        if display.display_status == 1:
+            display.draw_text('AircoPi standing by ...')
+            display.display_status = 0
+            print('Display status is now: ' + str(display.display_status))
         if scan_button.is_pressed:  # allows for scanning an IR code.
-            scan_result = scan_code()
-        if vibration_sensor.is_active:
+            scan_code()
+        if not magnetic_switch.is_active:
             print('Airco vibration detected. Starting airco logic')
             airco_running()
         if test_button.is_pressed:
